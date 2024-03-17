7c53cae48d9a7d5a7139bb92709a830162da86c0
Emil Lystimaki <emilaleksanteri@gmail.com>
2024-03-17 20:44:41 +0000

    test: 💍 extend tests

--- main_test.go
+++ main_test.go
@@ -21,3 +21,67 @@ func TestBasicSelect(t *testing.T) {
 		t.Errorf("Expected select * from table, got %s", parsed)
 	}
 }
+
+func TestBasicWhere(t *testing.T) {
+	parser := parser{input: "yoink * skibity table on god column = 5"}
+	parsed := parser.parse()
+	if parsed != "select * from table where column = 5" {
+		t.Errorf("Expected select * from table where column = 5, got %s", parsed)
+	}
+}
+
+func TestBasicAnd(t *testing.T) {
+	parser := parser{input: "yoink * skibity table on god column = 5 goon column2 = 6"}
+	parsed := parser.parse()
+	if parsed != "select * from table where column = 5 and column2 = 6" {
+		t.Errorf("Expected select * from table where column = 5 and column2 = 6, got %s", parsed)
+	}
+}
+
+func TestBasicOr(t *testing.T) {
+	parser := parser{input: "yoink * skibity table on god column = 5 edge column2 = 6"}
+	parsed := parser.parse()
+	if parsed != "select * from table where column = 5 or column2 = 6" {
+		t.Errorf("Expected select * from table where column = 5 or column2 = 6, got %s", parsed)
+	}
+}
+
+func TestBasicNot(t *testing.T) {
+	parser := parser{input: "yoink * skibity table on god bruh column = 5"}
+	parsed := parser.parse()
+	if parsed != "select * from table where not column = 5" {
+		t.Errorf("Expected select * from table where not column = 5, got %s", parsed)
+	}
+}
+
+func TestBasicAsc(t *testing.T) {
+	parser := parser{input: "yoink * skibity table short king"}
+	parsed := parser.parse()
+	if parsed != "select * from table asc" {
+		t.Errorf("Expected select * from table asc, got %s", parsed)
+	}
+}
+
+func TestBasicDesc(t *testing.T) {
+	parser := parser{input: "yoink * skibity table tall king"}
+	parsed := parser.parse()
+	if parsed != "select * from table desc" {
+		t.Errorf("Expected select * from table desc, got %s", parsed)
+	}
+}
+
+func TestBasicDelete(t *testing.T) {
+	parser := parser{input: "yeet skibity table on god column = 5"}
+	parsed := parser.parse()
+	if parsed != "delete from table where column = 5" {
+		t.Errorf("Expected delete from table where column = 5, got %s", parsed)
+	}
+}
+
+func TestBasicInsert(t *testing.T) {
+	parser := parser{input: "slide into table (column1, column2) values (5, 6)"}
+	parsed := parser.parse()
+	if parsed != "insert into table (column1, column2) values (5, 6)" {
+		t.Errorf("Expected insert into table (column1, column2) values (5, 6), got %s", parsed)
+	}
+}
